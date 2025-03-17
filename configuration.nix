a029db26f79b9ee2d1504374ad4cf8a531370c97
Luke Peterson <hazelnusse@gmail.com>
2025-03-16 18:11:36 -0700

    Enable home-manager and rebuild system
    For some reason, after this step I do not have a home-configuration.nix
    man page:
    
    [nix-shell:/etc/nixos]# man home-configuration.nix
    No manual entry for home-configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
@@ -87,6 +88,14 @@
     #  thunderbird
     ];
   };
+
+  home-manager = {
+  # also pass inputs to home-manager modules
+  extraSpecialArgs = { inherit inputs; };
+  users = {
+    "luke" = import ./home.nix;
+  };
+};
 
   # Install firefox.
   programs.firefox.enable = true;
