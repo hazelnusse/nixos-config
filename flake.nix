0a90e680eb117e4a0fcf911a89c76a625ed2c740
Luke Peterson <hazelnusse@gmail.com>
2025-05-28 16:32:41 -0700

    Rename nixosConfig and prepare for multiple configurations

--- flake.nix
+++ flake.nix
@@ -13,12 +13,14 @@
   outputs = { self, nixpkgs, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./hosts/t480s/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+      t480s = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/t480s/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
     };
   };
 }
